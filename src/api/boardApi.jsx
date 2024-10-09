09520f1bf8ebb6f41440cae3d0d633f4ddb60a08
Sung Hyobin <hellmir610@gmail.com>
2024-10-09 17:57:23 +0900

    feat(board): 게시글 등록 시 요청과 리다이렉트 형식 변경 (#6)
    - boardApi.jsx
    - AddByEditorComponent.jsx

--- src/api/boardApi.jsx
+++ src/api/boardApi.jsx
@@ -2,8 +2,7 @@ import {BOARD} from "./config.js";
 import axios from "axios";
 
 export const postAdd = async (addParam) => {
-    const header = {headers: {'Content-Type': 'multipart/form-data'}}
-    const res = await axios.post(`${BOARD}`, addParam, header)
+    const header = {headers: {'Content-Type': 'application/json'}}
 
-    return res.data
+    return await axios.post(`${BOARD}`, addParam, header)
 }
